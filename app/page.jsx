5a29cf11d388b168ece96fce9d6a1e25b3517d1d
NiklausRupail <m.rup.business@gmail.com>
2024-09-11 23:19:20 +0200

    Bug fixes

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,7 @@ const HomePage = () => {
   return (
     <>
       <section className={styles.homePage}>
-        <Image src={logo}></Image>
+        <Image alt='logo' src={logo}></Image>
         <h1>Całość stworzona dzięki NEXT.js</h1>
         <small>googlowanie nie boli{')'}</small>
         <h2>
